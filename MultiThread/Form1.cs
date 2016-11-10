da1395895139f3680e301d47c108f97e1531dbaa
DeniOSI <geksabyte@gmail.com>
2016-11-11 00:04:30 +0200

    init

---
+++ MultiThread/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiThread
+{
+    public delegate void Mydelegate(string str);
+    public partial class Form1 : Form
+    {
+        MyThread mt;
+        Mydelegate md;
+        MyThread mythread;
+        public Form1()
+        {
+            InitializeComponent();
+            //Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            md = Sd;
+            mythread = new MyThread(10, md, "first");
+            mt = new MyThread(10, md, "second");
+            mt.Run();
+            mythread.Run();
+
+
+        }
+        public void Sd(string str)
+        {
+            if(this.InvokeRequired)
+            {
+                this.Invoke(md, str);
+                return;
+            }
+            this.richTextBox1.AppendText( str + "\n" );
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            mt.Abort();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            mt.Pause();
+            mythread.Pause();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+           
+            mt.Restart();
+            mythread.Restart();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            mt.GetValueState();
+            listBox1.Items.Clear();
+            foreach (var item in mt.valuestate)
+
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+    }
+}
---
+++ MultiThread/MyThread.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace MultiThread
+{
+    class MyThread
+    {
+   public List<string> valuestate;
+        List<Thread> threadlist;
+        int _threadcount;
+        Mydelegate _md;
+        string _threadname;
+        public MyThread(int threadcount, Mydelegate md, string name)
+        {
+            _threadcount = threadcount;
+            _md = md;
+            _threadname = name;
+
+        }
+        public void Restart()
+        {
+            foreach(var value in threadlist)
+            {
+                if ( (int)value.ThreadState == 68 )
+                 value.Resume();
+            }
+        }
+        public void Pause()
+        {
+
+            foreach(var value in threadlist)
+            {
+               
+                value.Suspend();
+            }
+        }
+        public void Run()
+        {
+            threadlist = new List<Thread>();
+            for(int i=0; i< 10; i++)
+            {
+              var _thread = new Thread(new ThreadStart(MyFunc));
+                _thread.IsBackground = true;
+                threadlist.Add(_thread);
+                threadlist[i].Start();
+            }
+        }
+       
+        public void Abort()
+        {
+            foreach(var value in threadlist)
+            {
+                value.Abort();
+                
+            }
+            threadlist.Clear();
+        }
+        public void MyFunc()
+        {
+            int i = 0;
+            while(true)
+            {
+                _md(i.ToString()+_threadname);
+                i++;
+             Thread.Sleep(100);
+            }   
+        }
+        public void GetValueState()
+        {
+            valuestate = new List<string>();
+            foreach(var value in threadlist)
+            {
+                valuestate.Add(value.ThreadState.ToString());
+            }
+        }
+    }
+}
